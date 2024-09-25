630dd9850ac4e306436355b83958acbb9d39fe57
Miguel <miguelgomez29117@hotmail.com>
2024-09-25 12:57:41 -0500

    first commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,127 +1,47 @@
-<x-base-layout :scrollspy="false">
+<x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <x-slot:pageTitle>
-        {{-- {{$title}}  --}}
-    </x-slot>
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
 
-    <!-- BEGIN GLOBAL MANDATORY STYLES -->
-    <x-slot:headerFiles>
-        <!--  BEGIN CUSTOM STYLE FILE  -->
-        @vite(['resources/scss/light/assets/authentication/auth-boxed.scss'])
-        @vite(['resources/scss/dark/assets/authentication/auth-boxed.scss'])
-
-        <style>
-            #load_screen {
-                display: none;
-            }
-        </style>
-        <!--  END CUSTOM STYLE FILE  -->
-    </x-slot>
-    <!-- END GLOBAL MANDATORY STYLES -->
-    
-    <div class="auth-container d-flex">
-
-        <div class="container mx-auto align-self-center">
-    
-            <div class="row">
-    
-                <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-8 col-12 d-flex flex-column align-self-center mx-auto">
-                    <div class="card mt-3 mb-3">
-                        <div class="card-body">
-    
-                            <div class="row">
-                                <div class="col-md-12 mb-3">
-                                    
-                                    <h2>Sign In</h2>
-                                    <p>Enter your email and password to login</p>
-                                    
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="mb-3">
-                                        <label class="form-label">Email</label>
-                                        <input type="email" class="form-control">
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="mb-4">
-                                        <label class="form-label">Password</label>
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="mb-3">
-                                        <div class="form-check form-check-primary form-check-inline">
-                                            <input class="form-check-input me-3" type="checkbox" id="form-check-default">
-                                            <label class="form-check-label" for="form-check-default">
-                                                Remember me
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-                                
-                                <div class="col-12">
-                                    <div class="mb-4">
-                                        <button class="btn btn-secondary w-100">SIGN IN</button>
-                                    </div>
-                                </div>
-                                
-                                <div class="col-12 mb-4">
-                                    <div class="">
-                                        <div class="seperator">
-                                            <hr>
-                                            <div class="seperator-text"> <span>Or continue with</span></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                
-                                <div class="col-sm-4 col-12">
-                                    <div class="mb-4">
-                                        <button class="btn  btn-social-login w-100 ">
-                                            <img src="{{Vite::asset('resources/images/google-gmail.svg')}}" alt="" class="img-fluid">
-                                            <span class="btn-text-inner">Google</span>
-                                        </button>
-                                    </div>
-                                </div>
-    
-                                <div class="col-sm-4 col-12">
-                                    <div class="mb-4">
-                                        <button class="btn  btn-social-login w-100">
-                                            <img src="{{Vite::asset('resources/images/github-icon.svg')}}" alt="" class="img-fluid">
-                                            <span class="btn-text-inner">Github</span>
-                                        </button>
-                                    </div>
-                                </div>
-    
-                                <div class="col-sm-4 col-12">
-                                    <div class="mb-4">
-                                        <button class="btn  btn-social-login w-100">
-                                            <img src="{{Vite::asset('resources/images/twitter.svg')}}" alt="" class="img-fluid">
-                                            <span class="btn-text-inner">Twitter</span>
-                                        </button>
-                                    </div>
-                                </div>
-
-                                <div class="col-12">
-                                    <div class="text-center">
-                                        <p class="mb-0">Dont't have an account ? <a href="javascript:void(0);" class="text-warning">Sign Up</a></p>
-                                    </div>
-                                </div>
-                                
-                            </div>
-                            
-                        </div>
-                    </div>
-                </div>
-                
-            </div>
-            
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-    </div>
-    
-    <!--  BEGIN CUSTOM SCRIPTS FILE  -->
-    <x-slot:footerFiles>
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
 
-    </x-slot>
-    <!--  END CUSTOM SCRIPTS FILE  -->
-</x-base-layout>
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
+                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+            </label>
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+
+            <x-primary-button class="ms-3">
+                {{ __('Log in') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
--- resources/views/pages/poblacion.blade.php
+++ resources/views/pages/poblacion.blade.php
@@ -1,4 +1,4 @@
-<x-base-layout :scrollspy="false">
+<x-base-layout :scrollspy="true">
 
     <x-slot:pageTitle>
         {{$title}} 
@@ -6,178 +6,82 @@
 
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <x-slot:headerFiles>
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" />
-        <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"></script>
-        <link rel="stylesheet" href="{{asset('plugins/table/datatable/datatables.css')}}">
-        @vite(['resources/scss/light/plugins/table/datatable/dt-global_style.scss'])
-        @vite(['resources/scss/light/plugins/table/datatable/custom_dt_miscellaneous.scss'])
-        @vite(['resources/scss/dark/plugins/table/datatable/dt-global_style.scss'])
-        @vite(['resources/scss/dark/plugins/table/datatable/custom_dt_miscellaneous.scss'])
+        <!--  BEGIN CUSTOM STYLE FILE  -->
+        @vite(['resources/scss/light/assets/components/timeline.scss'])
         <!--  END CUSTOM STYLE FILE  -->
+        
+        <style>
+            .toggle-code-snippet { margin-bottom: 0px; }
+            body.dark .toggle-code-snippet { margin-bottom: 0px; }
+        </style>
     </x-slot>
     <!-- END GLOBAL MANDATORY STYLES -->
 
-    <style>
-        .custom-dropdown .dropdown-menu.show {
-            z-index: 1050;
-        }
-    </style>
+    <x-slot:scrollspyConfig>
+        data-bs-spy="scroll" data-bs-target="#navSection" data-bs-offset="100"
+    </x-slot>
+    
+    <!-- BREADCRUMB -->
+    <!-- /BREADCRUMB -->
 
     <div class="row layout-top-spacing">
-        <h3>Población</h3>
+        <h3>Generador de folios</h3>
     </div>
-
+    
     <div class="row">
-        <div class="col-xl-12 col-lg-12 col-sm-12 layout-spacing">
-            <div class="statbox widget box box-shadow">
-                <div class="widget-content widget-content-area">              
-                    <table class="multi-table table dt-table-hover" style="width:100%">
-                    <thead>
-                        <tr>
-                            <th>CURP</th>
-                            <th>Nombre completo</th>
-                            <th>Fecha de nacimiento</th>
-                            <th>Edad</th>
-                            <th>Dirección</th>
-                            <th class="text-center">Esquema</th>
-                            <th class="text-center dt-no-sorting">Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>AAAA000000AAAAAAA0</td>
-                            <td>Tiger Nixon</td>
-                            <td>00/00/0000</td>
-                            <td>00</td>
-                            <td>Municipio, localidad, colonia</td>
-                            <td>
-                                <div class="t-dot bg-success" data-bs-toggle="tooltip" data-bs-placement="top" data-original-title="Completo"></div>
-                            </td>
-                            <td class="text-center">
-                                <div class="dropdown custom-dropdown">
-                                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink1" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink1">
-                                        <a class="dropdown-item" href="javascript:void(0);">Ver esquema</a>
-                                        <a class="dropdown-item" href="javascript:void(0);">Deshabilitar</a>
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>BBBB111111BBBBBBB1</td>
-                            <td>Garrett Winters</td>
-                            <td>01/01/1991</td>
-                            <td>33</td>
-                            <td>Municipio, localidad, colonia</td>
-                            <td>
-                                <div class="t-dot bg-danger" data-bs-toggle="tooltip" data-bs-placement="top" data-original-title="Incompleto"></div>
-                            </td>
-                            <td class="text-center">
-                                <div class="dropdown custom-dropdown">
-                                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink2" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink2">
-                                        <a class="dropdown-item" href="javascript:void(0);">Ver esquema</a>
-                                        <a class="dropdown-item" href="javascript:void(0);">Deshabilitar</a>
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>CCCC222222CCCCCCC2</td>
-                            <td>Ashton Cox</td>
-                            <td>02/02/1982</td>
-                            <td>42</td>
-                            <td>Municipio, localidad, colonia</td>
-                            <td>
-                                <div class="t-dot bg-success" data-bs-toggle="tooltip" data-bs-placement="top" data-original-title="Completo"></div>
-                            </td>
-                            <td class="text-center">
-                                <div class="dropdown custom-dropdown">
-                                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink3" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink3">
-                                        <a class="dropdown-item" href="javascript:void(0);">Ver esquema</a>
-                                        <a class="dropdown-item" href="javascript:void(0);">Deshabilitar</a>
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>DDDD333333DDDDDDD3</td>
-                            <td>Cedric Kelly</td>
-                            <td>03/03/1973</td>
-                            <td>51</td>
-                            <td>Municipio, localidad, colonia</td>
-                            <td>
-                                <div class="t-dot bg-danger" data-bs-toggle="tooltip" data-bs-placement="top" data-original-title="Incompleto"></div>
-                            </td>
-                            <td class="text-center">
-                                <div class="dropdown custom-dropdown">
-                                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink4" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink4">
-                                        <a class="dropdown-item" href="javascript:void(0);">Ver esquema</a>
-                                        <a class="dropdown-item" href="javascript:void(0);">Deshabilitar</a>
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>EEEE444444EEEEEEE4</td>
-                            <td>Airi Satou</td>
-                            <td>04/04/1964</td>
-                            <td>60</td>
-                            <td>Municipio, localidad, colonia</td>
-                            <td>
-                                <div class="t-dot bg-success" data-bs-toggle="tooltip" data-bs-placement="top" data-original-title="Completo"></div>
-                            </td>
-                            <td class="text-center">
-                                <div class="dropdown custom-dropdown">
-                                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink5" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink5">
-                                        <a class="dropdown-item" href="javascript:void(0);">Ver esquema</a>
-                                        <a class="dropdown-item" href="javascript:void(0);">Deshabilitar</a>
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
 
-                        <tfoot>
-                            <tr>
-                                <th>CURP</th>
-                                <th>Nombre completo</th>
-                                <th>Fecha de nacimiento</th>
-                                <th>Edad</th>
-                                <th>Dirección</th>
-                                <th class="text-center">Esquema</th>
-                                <th class="text-center">Acciones</th>
-                            </tr>
-                        </tfoot>
-                    </table>
+        <div id="flHorizontalForm" class="col-lg-12 layout-spacing">
+            <div class="statbox widget box box-shadow">
+                <div class="widget-header">                                
+                    <div class="row">
+                        <div class="col-xl-12 col-md-12 col-sm-12 col-12">
+                            <h4>Porfavor rellene todos los campos correspondientes</h4>
+                        </div>                                                                        
+                    </div>
+                </div>
+                <div class="widget-content widget-content-area">
+                    <form>
+                        <div class="row mb-3">
+                          <label for="inputEmail2" class="col-sm-2 col-form-label">Generado por:</label>
+                          <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputEmail2">
+                          </div>
+                        </div>
+                        <div class="row mb-3">
+                          <label for="inputPassword2" class="col-sm-2 col-form-label">Fecha y hora:</label>
+                          <div class="col-sm-10">
+                            <input type="datetime-local" class="form-control" id="inputPassword2">
+                          </div>
+                        </div>
+                        <div class="row mb-3">
+                          <label for="inputEmail2" class="col-sm-2 col-form-label">Para:</label>
+                          <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputEmail2">
+                          </div>
+                        </div>
+                        <div class="row mb-3">
+                          <label for="inputEmail2" class="col-sm-2 col-form-label">Oficina o Delegación:</label>
+                          <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputEmail2">
+                          </div>
+                        </div>
+                        <div class="d-grid gap-2 col-6 mx-auto">
+<!-- Default -->
+<button class="btn btn-primary mb-4" type="button">Generar</button>
+
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
     </div>
 
+ 
+    
     <!--  BEGIN CUSTOM SCRIPTS FILE  -->
     <x-slot:footerFiles>
-        <script src="{{asset('plugins/global/vendors.min.js')}}"></script>
-        @vite(['resources/assets/js/custom.js'])
-        <script src="{{asset('plugins/table/datatable/datatables.js')}}"></script>
-        <script src="{{asset('plugins/table/datatable/button-ext/dataTables.buttons.min.js')}}"></script>
-        <script src="{{asset('plugins/table/datatable/button-ext/jszip.min.js')}}"></script>
-        <script src="{{asset('plugins/table/datatable/button-ext/buttons.html5.min.js')}}"></script>
-        <script src="{{asset('plugins/table/datatable/button-ext/buttons.print.min.js')}}"></script>
-        <script src="{{asset('plugins/table/datatable/custom_miscellaneous.js')}}"></script>
+
     </x-slot>
     <!--  END CUSTOM SCRIPTS FILE  -->
 </x-base-layout>
